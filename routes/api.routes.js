f02792aa31a311dc03980da61d0ac3c50e926d81
Nathalie Cazemajou <nat.caze@gmail.com>
2022-11-24 13:30:26 +0000

    created seeds folder

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
-
+const Cocktail = require("../models/Cocktail.model");
+const 
 //GET routes
-router.get("/api", (req, res, next) => {});
+router.get("/generated-cocktail", async (req, res, next) => {
+  try {
+    const generatedCocktail = await Cocktail.findOne();
+    res.status(200).json(generatedCocktail);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get("/api/cocktail-details/:id", (req, res, next) => {});
+router.get("/cocktail-details/:id", (req, res, next) => {});
 
 module.exports = router;
