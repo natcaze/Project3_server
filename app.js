a8d79dd3c8f95b84c1f71c50abce45e1348c1e40
Nathalie Cazemajou <nat.caze@gmail.com>
2022-11-24 11:43:16 +0000

    routes created

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use("/api", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
