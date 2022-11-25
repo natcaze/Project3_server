797f7e21da744905181e598d4a0144d49ba70b05
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-11-25 10:51:50 +0000

    GET and PUT user profile done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,32 +1,56 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
+
 
 //GET route
-router.get("/user/:id", (req, res, next) => { 
-    
+router.get("/profile/:id", async (req, res, next) => { 
+    try {
+      const {id} = req.params
+      const getUser = await User.findById(id)
+      
+      res.status(200).json(getUser)
+    } catch (error) {
+      next(error)
+    }
 });
 
 //PUT route
-router.put("/user/edit-profile/:id", (req, res, next) => {
+router.put("/edit-profile/:id", async (req, res, next) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    const { name, password } = req.body;
+    const updatedProfile = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        password,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedProfile);
+  } catch (error) {
+    next(error);
+  }
 });
 
+
 //GET route
-router.get("/user/favorites/:id", (req, res, next) => {});
+router.get("/favorites/:id", (req, res, next) => {});
 
 //POST route
-router.post("/user/create-favorite/:id", (req, res, next) => {
+router.post("/create-favorite/:id", (req, res, next) => {
   try {
   } catch (error) {}
 });
 
 //DELETE
-router.delete("/user/edit-favorites/:id", (req, res, next) => {
+router.delete("/edit-favorites/:id", (req, res, next) => {
   try {
   } catch (error) {}
 });
 
 //GET route
-router.get("/user/created-articles/:id", (req, res, next) => {});
+router.get("/created-articles/:id", (req, res, next) => {});
 
 module.exports = router;
