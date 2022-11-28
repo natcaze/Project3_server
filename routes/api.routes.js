3c22329a864865a041c9dfac1840492c3c66b595
Nathalie Cazemajou <nat.caze@gmail.com>
2022-11-28 17:33:25 +0000

    random cocktail route done

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -6,21 +6,17 @@ router.get("/generated-cocktail", async (req, res, next) => {
   try {
     const { ingredients } = req.query;
 
-let generatedCocktail
+    let generatedCocktail;
 
-    if (typeof ingredients === "object"){
-
-       generatedCocktail = await Cocktail.findOne({
+    if (typeof ingredients === "object") {
+      generatedCocktail = await Cocktail.findOne({
         strIngredient: { $all: [...ingredients] },
       });
-    }else {
-       generatedCocktail = await Cocktail.findOne({
+    } else {
+      generatedCocktail = await Cocktail.findOne({
         strIngredient: { $in: ingredients },
       });
-
     }
-
-
 
     res.status(200).json(generatedCocktail);
   } catch (error) {
@@ -34,6 +30,20 @@ router.get("/cocktail-details/:id", async (req, res, next) => {
 
     const detailsCocktail = await Cocktail.findById(id);
     res.status(200).json(detailsCocktail);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/random", async (req, res, next) => {
+  try {
+    const cocktailCount = await Cocktail.count({});
+    console.log(cocktailCount);
+    const randomNumber = Math.floor(Math.random() * cocktailCount);
+    const randomCocktail = await Cocktail.find()
+      .skip(randomNumber)
+      .limit(- 1);
+    res.status(200).json(randomCocktail);
   } catch (error) {
     next(error);
   }
